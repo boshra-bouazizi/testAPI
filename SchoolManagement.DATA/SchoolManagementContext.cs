086e8bdfa18ce6cc659d64d973597fff5d3826ff
Bochra Bouazizi <bouazizi.bochra@future-eye-inc.com>
2020-12-08 17:05:33 +0100

    added DBContext

---
+++ SchoolManagement.DATA/SchoolManagementContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolManagement.DOMAIN;
+
+namespace SchoolManagement.DATA
+{
+    public class SchoolManagementContext : DbContext
+    {
+        public SchoolManagementContext(DbContextOptions<SchoolManagementContext> options) : base (options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
